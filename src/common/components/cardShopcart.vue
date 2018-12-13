b6c1faa160d0c7f4d9c7fb4aad19f1ecf83ef785
djkong <djkong@corp.netease.com>
2018-12-13 21:34:21 +0800

    card good 50%

--- src/common/components/cardShopcart.vue
+++ src/common/components/cardShopcart.vue
@@ -5,6 +5,23 @@
       <!--<slot>-->
         <div class="item-preview">
           <span v-if="product_img" class="item-image" :style="{'background-image':'url('+product_img+')'}"></span>
+          <div class="item-desc">
+            <p class="item-price"><span>￥</span>{{info.price}}</p>
+            <p class="item-inventory">库存数量 {{info.inventory}}件</p>
+          </div>
+        </div>
+        <div class="item-taste">
+          <p class="title">口味</p>
+          <div class="list-taste">
+            <span class="selected">啦啦啦</span>
+            <span>啦啦啦</span>
+            <span>啦啦啦啦啦啦</span>
+            <span>啦啦啦</span>
+            <span>啦啦啦啦啦啦</span>
+            <span>啦啦啦</span>
+            <span>啦啦啦</span>
+            <span>啦啦啦啦啦啦</span>
+          </div>
         </div>
       <!--</slot>-->
     </div>
@@ -41,23 +58,75 @@
     },
     methods: {
       clickClose(){
-        this.hide = true;
-        setTimeout(()=>{
-          this.$emit('close');
-        },500);
-      }
+      this.hide = true;
+      setTimeout(()=>{
+         this.$emit('close');
+      },500);
     }
   }
+}
 </script>
 <style scoped>
-  @import "../wxss/card.css";
-  .card-shopcart{
-    .item-preview{
+@import "../wxss/card.css";
+.card-shopcart{
+  .item-preview{
       display: flex;
+      padding-left: 20rpx;
+      padding-bottom: 35rpx;
       .item-image{
-        width: 150rpx;
-        height: 150rpx;
+          flex: none;
+          width: 150rpx;
+          height: 150rpx;
+          border-radius: 8rpx;
+          background: var(--color-card-border) no-repeat center;
+          background-size: cover;
+      }
+      .item-desc{
+          flex: auto;
+          padding-left: 30rpx;
+          display: flex;
+          flex-direction:column;
+          justify-content: flex-end;
+          .item-price{
+              font-weight: bold;
+              font-size: 34rpx;
+              color: var(--color-text-selected);
+              span{
+                  font-size: 20rpx;
+              }
+          }
+          .item-inventory{
+              font-size: 22rpx;
+              color: var(--color-text-sub);
+          }
+      }
+  }
+  .item-taste{
+    display: flex;
+    flex-direction:column;
+    .title{
+      font-size: 24rpx;
+      color: var(--color-text-sub);
+    }
+    .list-taste{
+      display: flex;
+      flex-wrap: wrap;
+      padding-top: 5rpx;
+      > span{
+        font-size: 26rpx;
+        flex: none;
+        margin: 10rpx 25rpx 10rpx 0;
+        border: 1rpx solid var(--color-background);
+        background: var(--color-background);
+        padding: 5rpx 20rpx;
+        border-radius: 40rpx;
+        &.selected{
+          background: var(--color-tips-background-red);
+          border-color: var(--color-text-selected);
+          color: var(--color-text-selected);
+        }
       }
     }
   }
+}
 </style>
