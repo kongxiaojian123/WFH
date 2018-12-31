809c3b05f6f76fdfddd2089bc7498785a431acc2
vivian <tianwenwen555@sina.com>
2018-12-31 15:50:57 +0800

    order page url

---
+++ src/pages/order/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container" @click="">
+    <bar-menu :menuIndex="menuIndex" @selectMenu="selectMenu"/>
+    <view-recommend v-if="menuIndex===0"/>
+    <view-goods v-else-if="menuIndex===1"/>
+  </div>
+</template>
+
+<script>
+import BarMenu from '../../common/components/barMenu.vue'
+import ViewGoods from '../../common/components/viewGoods.vue'
+import ViewRecommend from '../../common/components/viewRecommend'
+export default {
+  onHide(){
+  },
+  onShow() {
+  },
+  components: {
+    BarMenu, ViewGoods,ViewRecommend
+  },
+  data () {
+    return {
+      menuIndex: 0
+    }
+  },
+  methods: {
+    selectMenu (index) {
+      this.menuIndex = index
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
