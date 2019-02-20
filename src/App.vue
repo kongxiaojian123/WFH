4775ef613a42a3b22c3568ba8d6e7a205076e661
djkong <djkong@corp.netease.com>
2019-02-20 20:04:15 +0800

    font family

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,4 @@ export default {
   @import 'common/wxss/icon.css';
   @import 'common/wxss/color.css';
   @import 'common/wxss/common.css';
-  page{
-    font-size: 26rpx;
-    background: var(--color-background);
-    color: var(--color-text);
-  }
 </style>
